4758d50a07628ab2c320f782c4b8a49ea2131ce2
Michael Tang <michael.tang@ubc.ca>
2025-09-09 09:57:30 -0700

    fixed syntax error

--- site/profile/manifests/openondemand.pp
+++ site/profile/manifests/openondemand.pp
@@ -2,7 +2,7 @@ class profile::openondemand {
   include openondemand
   
   @consul::service { 'ondemand':
-    port => ${openondemand::custom_port},
+    port => $openondemand::custom_port,
     tags => ['ondemand'],
   }
 }
